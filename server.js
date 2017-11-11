44e918a0275832719716d77834262d331811b14d
Victor H. Heclis <vheeclis@gmail.com>
2017-11-10 22:39:53 -0200

    Pequenas mudanças estruturais
    Fiz algumas mudanças em como está o código para ficar mais perto dos padrões

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const { body, validationResult } = require('express-validator/check');
-var express = require('express'),
+let express = require('express'),
      app = express(),
      port = 8080;
 
-var body_parser = require('body-parser');
+let body_parser = require('body-parser');
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
@@ -12,21 +12,21 @@ Payment = require('./db.js')
 
 
 
-app.get('/plans', function(req, res)
- {
+app.get('/plans', (req, res) => {
     res.contentType('application/json');
-    var answer = [ {
-      'Plano Gold' :
+    let answer = [ {
+      //coloquei os products como chaves e o campo 'name' para que a chave não tivesse espaço
+      'gold_plan' :
         {
-          product : 'gold_plan', price : 'R$ 59,90', description : 'plano pago gold'
+          name : 'Plano Gold', price : 'R$ 59,90', description : 'plano pago gold'
         },
-      'Plano Platinum' :
+      'platinum_plan' :
         {
-          product : 'platinum_plan', price : 'R$ 79,90', description : 'premium platinum'
+          name : 'Plano Platinum', price : 'R$ 79,90', description : 'premium platinum'
         },
-      'Plano Super Premium' :
+      'super_premium_plan' :
         {
-          product : 'super_premium_plan', price : 'R$ 129,90', description : ' o melhor plano de todos'
+          name : 'Plano Super Premium', price : 'R$ 129,90', description : ' o melhor plano de todos'
         }
     } ]
     res.status(200);
@@ -43,58 +43,60 @@ app.post('/payment',
             body('discount').exists(),
             body('transaction_id').exists()
           ],
-          function(req, res)
-            {
+          (req, res) => {
               if(!validationResult(req).isEmpty())
-                return res.status(406).send("ERRO: Todos os campos devem ser preenchidos");
+                return res.status(400).send("ERRO: Todos os campos devem ser preenchidos");
               /* Regex feito para conseguir pegar um número real sem me importar
                * com Strings que acompanhem esse dado.
                */
-              var realNumber = new RegExp('(\\d*[.])?\\d+');
+              let realNumber = new RegExp('(\\d*[.])?\\d+');
 
               /* Nas próximas duas variáveis eu transformei as ',' em '.' para
                * poder usar parseFloat corretamente e usei o RegExp para
                * conseguir pegar o número, independente de como foi feita a
                * input.
                */
-              var product_price = req.body.product_price.replace(',','.').match(realNumber)[0];
-              var discount = req.body.discount.replace(',','.').match(realNumber)[0];
-              var payment_date = req.body.payment_date
-              var payment_type = req.body.payment_type;
-              var product = req.body.product;
-              var transaction_id = req.body.transaction_id;
+              let product_price = req.body.product_price.replace(',','.').match(realNumber)[0];
+              let discount = req.body.discount.replace(',','.').match(realNumber)[0];
+              let payment_date = req.body.payment_date
+              let payment_type = req.body.payment_type;
+              let product = req.body.product;
+              let transaction_id = req.body.transaction_id;
 
 
-
-              if (discount > 50.00)
-                return res.status(406).send("ERRO: O desconto oferecido não pode ser superior a 50%");
+              let max_discount = 50.00
+              if (discount > max_discount)
+                return res.status(400).send("ERRO: O desconto oferecido não pode ser superior a 50%");
 
               switch(product)
               {
                 case 'gold_plan':
-
-                if(product_price != 59.90)
-                    return res.status(406).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$59,90")
+                let price_gold = 59.90;
+                if(product_price != price_gold)
+                    return res.status(400).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$" + price_gold)
 
                 break;
                 case 'platinum_plan':
-                if(product_price != 79.90)
-                    return res.status(406).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$79,90")
+                let platinum_price = 79.90;
+                if(product_price != platinum_price)
+                    return res.status(400).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$" + platinum_price)
 
                 break;
                 case 'super_premium_plan':
-                if(product_price != 129.90)
-                    return res.status(406).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$129,90")
+                let super_premium_price = 129.90;
+                if(product_price != super_premium_price)
+                    return res.status(400).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$" + super_premium_price)
 
                 break;
                 default:
-                  return res.status(406).send("ERRO: O produto deve exisir. Deve ser um dos seguintes:\ngold_plan\nplatinum_plan\nsuper_premium_plan")
+                  return res.status(400).send("ERRO: O produto deve exisir. Deve ser um dos seguintes:\ngold_plan\nplatinum_plan\nsuper_premium_plan")
               }
-
-              var price = parseFloat(product_price) - ( (parseFloat(discount)/100)*parseFloat(product_price) );
+              let product_price_f = parseFloat(product_price);
+              let discount_f = parseFloat(discount);
+              let price = product_price_f - ( (discount_f/100)*product_price_f );
               
 
-              var payment = Payment({
+              let payment = Payment({
                 db_payment_date   : payment_date,
                 db_payment_type   : payment_type,
                 db_product        : product,
