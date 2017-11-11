6091f719c86cf37585cae043989fe4999af1e3c6
Victor H. Heclis <vheeclis@gmail.com>
2017-11-11 20:28:18 -0200

    Substituição do switch

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 let Plans =  {
+		'plans' :	{
       //coloquei os products como chaves e o campo 'name' para que a chave não tivesse espaço
       'gold_plan' :
         {
-          name : 'Plano Gold', price : 'R$ 59,90', description : 'plano pago gold'
+          name : 'Plano Gold', price : 59.90, description : 'plano pago gold'
         },
       'platinum_plan' :
         {
-          name : 'Plano Platinum', price : 'R$ 79,90', description : 'premium platinum'
+          name : 'Plano Platinum', price : 79.90, description : 'premium platinum'
         },
       'super_premium_plan' :
         {
-          name : 'Plano Super Premium', price : 'R$ 129,90', description : ' o melhor plano de todos'
-        },
-      'max_discount' : '50.00'
+          name : 'Plano Super Premium', price : 129.90, description : ' o melhor plano de todos'
+        }
+    	},
+    'max_discount' : 50.00
     };
 
 
--- server.js
+++ server.js
@@ -9,29 +9,30 @@ app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
 
 let Payment = require('./db.js')
-let Plans = require('./config.js');
+const Config = require('./config.js');
+let plan_names = Object.keys(Config.plans)
+
+/*  
+ *  A linha seguinte faz uma cópia do que está no Config.plans
+ *  de uma forma que alterar get_answear não alterará Config.plans.
+ */
+let get_answear = JSON.parse(JSON.stringify(Config.plans));
+for (let i in plan_names)
+      get_answear[plan_names[i]].price = 'R$ ' + get_answear[plan_names[i]].price.toString()
 
 app.get('/plans', (req, res) => {
-    res.contentType('application/json');
-    let answer = [ {
-      //coloquei os products como chaves e o campo 'name' para que a chave não tivesse espaço
-      'gold_plan' :
-        {
-          name : Plans.gold_plan['name'], price : Plans.gold_plan['price'] , description : Plans.gold_plan['description']
-        },
-      'platinum_plan' :
-        {
-          name : Plans.platinum_plan['name'], price : Plans.platinum_plan['price'] , description : Plans.platinum_plan['description']
-        },
-      'super_premium_plan' :
-        {
-          name : Plans.super_premium_plan['name'] , price : Plans.super_premium_plan['price'] , description : Plans.super_premium_plan['description']
-        }
-    } ]
+    res.contentType('application/json'); 
     res.status(200);
-    res.json(answer);
+    res.json(get_answear);
   }
 )
+
+ /*
+  * Para checar se todas as variáveis foram preenchidas eu faço uso 
+  * do express-validator, na função abaixo. Podemos ver que eu coloco
+  * uma regra de que deve existir cada um dos atributos no req.body
+  * através dos comandos body(<variavel>).exists().
+  */
 
 app.post('/payment',
           [
@@ -43,14 +44,21 @@ app.post('/payment',
             body('transaction_id').exists()
           ],
           (req, res) => {
+               /*
+                * A próxima linha faz o teste se todas as regras inseridas no 
+                * express-validator foram respeitadas, validationResult é vazio
+                * se todas as regras forem respeitadas.
+                */
               if(!validationResult(req).isEmpty())
                 return res.status(400).send("ERRO: Todos os campos devem ser preenchidos");
-              /* Regex feito para conseguir pegar um número real sem me importar
+              /* 
+               * Regex feito para conseguir pegar um número real sem me importar
                * com Strings que acompanhem esse dado.
                */
               let realNumber = new RegExp('(\\d*[.])?\\d+');
 
-              /* Nas próximas duas variáveis eu transformei as ',' em '.' para
+              /* 
+               * Nas próximas duas variáveis eu transformei as ',' em '.' para
                * poder usar parseFloat corretamente e usei o RegExp para
                * conseguir pegar o número, independente de como foi feita a
                * input.
@@ -63,53 +71,43 @@ app.post('/payment',
               let transaction_id = req.body.transaction_id;
 
 
-              let max_discount = Plans.max_discount;
+              let max_discount = Config.max_discount;
               if (discount > max_discount)
                 return res.status(400).send("ERRO: O desconto oferecido não pode ser superior a " + max_discount + "%");
 
-              switch(product)
+
+              for(let i in plan_names)
               {
-                case 'gold_plan':
-                let price_gold = Plans.gold_plan['price'].replace(',','.').match(realNumber)[0];
-                if(product_price != price_gold)
-                    return res.status(400).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$" + price_gold)
+                if(product == plan_names[i])
+                {
+                  let right_price = Config.plans[plan_names[i]]['price']
+                  if(product_price != right_price)
+                    return res.status(400).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$ " + right_price);
+                  let product_price_f = parseFloat(product_price);
+                  let discount_f = parseFloat(discount);
+                  let price = product_price_f - ( (discount_f/100)*product_price_f );
+              
+                  let payment = Payment({
+                    db_payment_date   : payment_date,
+                    db_payment_type   : payment_type,
+                    db_product        : product,
+                    db_product_price  : product_price,
+                    db_discount       : discount,
+                    db_transaction_id : transaction_id
+                  })
 
-                break;
-                case 'platinum_plan':
-                let platinum_price = Plans.platinum_plan['price'].replace(',','.').match(realNumber)[0];
-                if(product_price != platinum_price)
-                    return res.status(400).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$" + platinum_price)
+                  payment.save(function(err){
+                    if(err) return res.status(500).send("ERRO: Erro ao inserir dados no banco de dados");
 
-                break;
-                case 'super_premium_plan':
-                let super_premium_price = Plans.super_premium_plan['price'].replace(',','.').match(realNumber)[0];
-                if(product_price != super_premium_price)
-                    return res.status(400).send("ERRO: O preço do produto não é compativel com o produto, deveria ser R$" + super_premium_price)
-
-                break;
-                default:
-                  return res.status(400).send("ERRO: O produto deve exisir. Deve ser um dos seguintes:\ngold_plan\nplatinum_plan\nsuper_premium_plan")
+                    res.status(200).send("Pagamento registrado com sucesso")
+                  })
+                  return ;
+                }
               }
-              let product_price_f = parseFloat(product_price);
-              let discount_f = parseFloat(discount);
-              let price = product_price_f - ( (discount_f/100)*product_price_f );
-              
-
-              let payment = Payment({
-                db_payment_date   : payment_date,
-                db_payment_type   : payment_type,
-                db_product        : product,
-                db_product_price  : product_price,
-                db_discount       : discount,
-                db_transaction_id : transaction_id
-              })
-
-              
-              payment.save(function(err){
-                if(err) return res.status(500).send("ERRO: Erro ao inserir dados no banco de dados");
-
-                res.status(200).send("Pagamento registrado com sucesso")
-              })
+              let err_answear = "ERRO: O produto deve exisir. Deve ser um dos seguintes:\n";
+              for (let i in plan_names)
+                err_answear += plan_names[i] + "\n"
+              return res.status(400).send(err_answear);
               
             }
 )
